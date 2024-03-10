9ef3720948d463f20e85d3d8209027d261d42e31
dev <dev@mail.ru>
2024-03-10 20:45:11 +0700

    MainActivity

--- app/src/main/java/com/example/wewatch_l1/MainActivity.kt
+++ app/src/main/java/com/example/wewatch_l1/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.wewatch_l1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var imageEmpty: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageEmpty = findViewById(R.id.no_movies_layout)
+        val deleteBtn = findViewById<ImageView>(R.id.img_delete)
+
+        val addBtn = findViewById<FloatingActionButton>(R.id.fab)
+        addBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddActivity::class.java)
+            startActivityForResult(intent, ADD_VIEW_ACTIVITY_REQUEST_CODE)
+        }
+
+
+        deleteBtn.setOnClickListener {
+
+
+        }
+    }
+
+    companion object {
+        const val ADD_VIEW_ACTIVITY_REQUEST_CODE = 1
+    }
+
+    fun showToast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show()
     }
 }
